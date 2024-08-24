b4da3e77837a4cc729cd47eafe8a96ff51bfdd7c
Logan Edwards <139167799+logan-edwards@users.noreply.github.com>
2024-08-24 15:37:57 -0400

    Create helpers.h

---
+++ helpers.h
@@ -0,0 +1,7 @@
+#ifndef HELPERS_DEF
+#define HELPERS_DEF
+
+void plot_1d(float x0, float dx, int nx, float* y, char* label);
+void plot_2d(float x0, float dx, int nx, float y0, float dy, int ny, float* z, char* label);
+
+#endif
