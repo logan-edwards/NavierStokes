832ad7cb719ebf574ad9395f4aeb4df8685b8256
Logan Edwards <139167799+logan-edwards@users.noreply.github.com>
2024-08-24 15:38:14 -0400

    Create exercises.h

---
+++ exercises.h
@@ -0,0 +1,17 @@
+#ifndef EXERCISES_DEF
+#define EXERCISES_DEF
+
+void exercise_1(); // 1D Linear Convection
+void exercise_2(); // 1D Nonlinear Convection
+void exercise_3(); // 1D Diffusion
+void exercise_4();
+void exercise_5();
+void exercise_6();
+void exercise_7();
+void exercise_8();
+void exercise_9();
+void exercise_10();
+void exercise_11();
+void exercise_12();
+
+#endif
